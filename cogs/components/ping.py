13aa1c235058285b1fcaad2d21d84457d4fdb765
Geroge Rupp <a0919329@gmail.com>
2022-05-17 02:23:49 +0800

    QoC

---
+++ cogs/components/ping.py
@@ -0,0 +1,24 @@
+import json
+
+import interactions
+
+with open("./configuration/scope.json") as server_scopes:
+    server_scopes = json.load(server_scopes)
+    production = server_scopes["Production"]
+    testing = server_scopes["Testing"]
+
+class ouen(interactions.Extension):
+    def __init__(self, ArisaInteraction):
+        self.ArisaInteraction = ArisaInteraction
+        self.MESSAGE_CACHE = None
+
+    @interactions.extension_command(
+        name="ping",
+        description="回傳延遲時間",
+        scope=[production, testing]
+    ) 
+    async def ouen(self, ctx):
+        await ctx.send(content=f"pong!\n{round(self.Arisa.latency*1000)} ms")
+    
+def setup(ArisaInteraction):
+    ouen(ArisaInteraction)
