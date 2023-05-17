be763d28ed1f90c096871c138a9cca6ffde4bd8b
LTurret <a0919329@gmail.com>
2023-05-18 00:19:27 +0800

    small improvement of ai chat

--- cogs/ask.py
+++ cogs/ask.py
@@ -1,38 +1,42 @@
-import interactions
+import asyncio
+
+from interactions import Extension, slash_command, slash_option, SlashContext, OptionType
 import openai
 
 from core.scopes import scopes
 from core.secrets import tokens
 
-class ask(interactions.Extension):
-    def __init__(self, ArisaInteraction):
-        self.ArisaInteraction = ArisaInteraction
+async def chat(question: str):
+    openai.api_key = tokens()["openai"]
+    completion = await openai.ChatCompletion.acreate(
+        model = "gpt-3.5-turbo",
+        messages = [{
+                "role": "user",
+                "content": f"你現在是一位活潑的女孩子，叫做「松田亞利沙」，接下來的問題請以繁體中文回答：「{question}」"
+            }])
+    return completion
 
-    @interactions.extension_command(
+class ask(Extension):
+    def __init__(self, bot):
+        self.ArisaInteraction = bot
+        self.CHANNEL_CACHE = None
+
+    @slash_command(
         name = "ask",
         description = "問亞利沙",
-        scope = scopes()["all"],
-        options = [
-            interactions.Option(
-                name = "question",
-                description = "想要問的問題",
-                type = interactions.OptionType.STRING,
-                required = True
-            )
-        ]
+        scopes = scopes()["all"]
     )
-    async def shy(self, ctx: interactions.CommandContext, question: str):
-        openai.api_key = tokens()["openai"]
-        completion = openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo",
-            messages = [
-                    {
-                        "role": "user",
-                        "content": f"你現在是一個很活潑的16歲女孩，請用繁體中文回答以下問題：「{question}」"
-                    }
-                ]
-            )
-        await ctx.send(content=completion.choices[0].message.content)
-    
+    @slash_option(
+        name = "question",
+        description = "想要問的問題",
+        required = True,
+        opt_type = OptionType.STRING
+    )
+    async def ask(self, ctx: SlashContext, question: str):
+        self.CHANNEL_CACHE = ctx.channel
+        message = await asyncio.gather(asyncio.create_task(chat(question)))
+        content = message[0]["choices"][0]["message"]["content"]
+        await self.CHANNEL_CACHE.send(content=content)
+
 def setup(ArisaInteraction):
     ask(ArisaInteraction)
