ebb876f1f0fdd1e061e7dc533520c8209a0742af
lturret <a0919329@gmail.com>
2023-05-17 09:36:54 +0000

    replace ask.py to openai chat-completion

--- cogs/ask.py
+++ cogs/ask.py
@@ -1,6 +1,8 @@
+import json
 import random
 
 import interactions
+import openai
 
 from core.scopes import Scopes
 
@@ -22,9 +24,19 @@ class ask(interactions.Extension):
         ]
     )
     async def shy(self, ctx: interactions.CommandContext, question: str):
-        choice = ["可以！", "<:ArisaShy:957861166082822195>", "亞利沙覺得不行..."]
-        message = f"> {question}\n{random.choice(choice)}"
-        await ctx.send(content=message)
+        with open("./config/openai.json", "r") as token:
+            token = json.load(token)["token"]
+        openai.api_key = token
+        completion = openai.ChatCompletion.create(
+                model = "gpt-3.5-turbo",
+                messages = [
+                        {
+                            "role": "user",
+                            "content": f"你現在是一個很活潑的16歲女孩，請用繁體中文回答以下問題：「{question}」"
+                        }
+                    ]
+                )
+        await ctx.send(content=completion.choices[0].message.content)
     
 def setup(ArisaInteraction):
     ask(ArisaInteraction)
