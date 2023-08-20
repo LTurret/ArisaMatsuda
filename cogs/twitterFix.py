f36c69c085023a4b659fe22203a703c518bf2074
LTurret <a0919329@gmail.com>
2023-08-20 17:02:43 +0800

    code optimise - move headers into member function

--- cogs/twitterFix.py
+++ cogs/twitterFix.py
@@ -42,9 +42,37 @@ class twitterFix(Extension):
                 return {"bearer_token": bearer_token, "guest_token": guest_token}
 
             # Tweet fetch
-            async def fetch_tweet(tweetId: int, features: dict, variables: dict, query_id_token: str = "0hWvDhmW8YQ-S_ib3azIrw") -> dict:
-                variables: dict = {**variables}
-                variables["tweetId"] = tweetId
+            async def fetch_tweet(tweetId: int, query_id_token: str = "0hWvDhmW8YQ-S_ib3azIrw") -> dict:
+                features: dict = {
+                    "responsive_web_graphql_exclude_directive_enabled": True,
+                    "verified_phone_label_enabled": False,
+                    "responsive_web_graphql_timeline_navigation_enabled": True,
+                    "responsive_web_graphql_skip_user_profile_image_extensions_enabled": False,
+                    "tweetypie_unmention_optimization_enabled": True,
+                    "vibe_api_enabled": False,
+                    "responsive_web_edit_tweet_api_enabled": True,
+                    "graphql_is_translatable_rweb_tweet_is_translatable_enabled": True,
+                    "view_counts_everywhere_api_enabled": False,
+                    "longform_notetweets_consumption_enabled": True,
+                    "tweet_awards_web_tipping_enabled": False,
+                    "freedom_of_speech_not_reach_fetch_enabled": True,
+                    "standardized_nudges_misinfo": True,
+                    "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": True,
+                    "interactive_text_enabled": False,
+                    "responsive_web_twitter_blue_verified_badge_is_enabled": True,
+                    "responsive_web_text_conversations_enabled": False,
+                    "longform_notetweets_richtext_consumption_enabled": False,
+                    "responsive_web_enhance_cards_enabled": False,
+                    "longform_notetweets_rich_text_read_enabled": True,
+                    "longform_notetweets_inline_media_enabled": True,
+                    "responsive_web_media_download_video_enabled": False,
+                    "responsive_web_twitter_article_tweet_consumption_enabled": True,
+                    "creator_subscriptions_tweet_preview_api_enabled": True,
+                }
+                variables: dict = {"includePromotedContent": False, "withCommunity": False, "withVoice": False}
+
+                variables_reference: dict = {**variables}
+                variables_reference["tweetId"] = tweetId
 
                 root: str = "https://twitter.com/i/api"
                 prefix: str = "graphql"
@@ -138,40 +166,14 @@ class twitterFix(Extension):
                 return embed
 
             # API headers
-            features: dict = {
-                "responsive_web_graphql_exclude_directive_enabled": True,
-                "verified_phone_label_enabled": False,
-                "responsive_web_graphql_timeline_navigation_enabled": True,
-                "responsive_web_graphql_skip_user_profile_image_extensions_enabled": False,
-                "tweetypie_unmention_optimization_enabled": True,
-                "vibe_api_enabled": False,
-                "responsive_web_edit_tweet_api_enabled": True,
-                "graphql_is_translatable_rweb_tweet_is_translatable_enabled": True,
-                "view_counts_everywhere_api_enabled": False,
-                "longform_notetweets_consumption_enabled": True,
-                "tweet_awards_web_tipping_enabled": False,
-                "freedom_of_speech_not_reach_fetch_enabled": True,
-                "standardized_nudges_misinfo": True,
-                "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": True,
-                "interactive_text_enabled": False,
-                "responsive_web_twitter_blue_verified_badge_is_enabled": True,
-                "responsive_web_text_conversations_enabled": False,
-                "longform_notetweets_richtext_consumption_enabled": False,
-                "responsive_web_enhance_cards_enabled": False,
-                "longform_notetweets_rich_text_read_enabled": True,
-                "longform_notetweets_inline_media_enabled": True,
-                "responsive_web_media_download_video_enabled": False,
-                "responsive_web_twitter_article_tweet_consumption_enabled": True,
-                "creator_subscriptions_tweet_preview_api_enabled": True,
-            }
-            variables: dict = {"includePromotedContent": False, "withCommunity": False, "withVoice": False}
             tokens: dict = {**(await get_tokens())}
 
             # Find activation
             if search(r"https://twitter.com/", event.message.content) and search(r"/status/", event.message.content):
                 tweetId = search(r"status\/([0-9][^\?|\/]+)", event.message.content).group(1)
-                api_callback: dict = await fetch_tweet(tweetId, features, variables)
+                api_callback: dict = await fetch_tweet(tweetId)
                 content: dict = {**(await get_contents(api_callback))}
+                
                 init_embed: Embed = Embed(description=content["full_text"], color=0x1DA0F2, timestamp=time(), url="https://arisahi.me")
                 init_embed.set_author(
                     name=f"{content['author']} (@{content['screen_name']})", url=f"https://twitter.com/{content['screen_name']}", icon_url=content["icon_url"]
@@ -185,6 +187,7 @@ class twitterFix(Extension):
 
                 embeds: list = [init_embed]
 
+                # Embeds composer - used for multiple images
                 if content["images"]:
                     for image in content["images"]:
                         embeds.append(embed_generator(content, image))
