5bc4ea1a076444a35fbc63db52f99863b025ee29
LTurret <a0919329@gmail.com>
2023-08-21 17:04:23 +0800

    formatted

--- main.py
+++ main.py
@@ -9,19 +9,17 @@ from interactions import Activity
 from interactions import Client
 from interactions import Intents
 
+
 load_dotenv()
 
-arisa = Client(
-    intents = Intents.ALL,
-    activity = Activity(
-        name = "偶像大師 百萬人演唱會！ 劇場時光"
-    )
-)
+arisa = Client(intents=Intents.ALL, activity=Activity(name="偶像大師 百萬人演唱會！ 劇場時光"))
+
 
 @listen()
 async def on_startup():
     system("clear")
     print(f"Up!10sion♪\nEverybody attention!!")
+
 
 for filename in listdir("./cogs"):
     if filename.endswith(".py"):
