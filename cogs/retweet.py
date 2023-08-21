52547aee5bb8ed64ce75fdf038c11a6b2d015e0b
LTurret <a0919329@gmail.com>
2023-08-21 20:16:19 +0800

    add retweet function

--- cogs/retweet.py
+++ cogs/retweet.py
@@ -1,25 +1,85 @@
-# from interactions import events
+from json import loads
+from re import findall
+from time import time
+from os import getenv
+
+from aiohttp import ClientSession
+from bs4 import BeautifulSoup as bs
+
 from interactions import listen
-# from interactions import AllowedMentions
-# from interactions import Embed
+from interactions import Embed
 from interactions import Extension
-# from interactions import File
 from interactions import IntervalTrigger
 from interactions import Task
+
+from cogs.src.embed_generator import embed_generator
+from cogs.src.fetch_tweet import fetch_tweet
+from cogs.src.get_contents import get_contents
+from cogs.src.get_tokens import get_tokens
 
 
 class retweet(Extension):
     def __init__(self, Arisa):
         self.Arisa = Arisa
         print(f" ↳ Extension {__name__} created")
+        self.max_ptr = 0
 
     @listen()
     async def on_startup(self):
         self.retweet.start()
 
-    @Task.create(IntervalTrigger(seconds=2))
+    @Task.create(IntervalTrigger(seconds=120))
     async def retweet(self):
-        print("hello world")
+        user = "imasml_theater"
+        cookies = {"auth_token": getenv("auth_token")}
+        url = f"https://syndication.twitter.com/srv/timeline-profile/screen-name/{user}"
+        async with ClientSession(cookies=cookies) as session:
+            async with session.get(url) as response:
+                response = await response.text()
+
+        html = bs(response, "html.parser")
+        data = str(html.select("#__NEXT_DATA__")[0])
+        data = findall(r">({.+)[<]", data)[0]
+        data = loads(data)
+
+        timeline = data["props"]["pageProps"]["timeline"]["entries"]
+        timeline.reverse()
+        queue = []
+        for element in timeline:
+            tweet = element["content"]["tweet"]
+            if int(tweet["conversation_id_str"]) > self.max_ptr:
+                self.max_ptr = int(tweet["conversation_id_str"])
+                queue.append(tweet["conversation_id_str"])
+
+        tokens: dict = {**(await get_tokens())}
+        queue.reverse()
+        while queue:
+            tweetId = queue.pop()
+            api_callback: dict = await fetch_tweet(tokens, tweetId)
+            content: dict = {**(await get_contents(api_callback))}
+            init_embed: Embed = Embed(description=content["full_text"], color=0xD8A804, timestamp=time(), url="https://arisahi.me")
+            init_embed.set_author(
+                name=f"{content['author']} (@{content['screen_name']})", url=f"https://twitter.com/{content['screen_name']}", icon_url=content["icon_url"]
+            )
+            init_embed.add_field(name="原文傳送門", value=f"[點我](https://twitter.com/{user}/status/{tweetId})", inline=False)
+            init_embed.set_footer(
+                text="樓梯的轉推百萬魔法",
+                icon_url="https://file.notion.so/f/s/5afa187d-3390-4337-823f-692939c724aa/imas_theater_icon.png?id=258485b3-6581-4f1b-af38-42fd0367c4cf&table=block&spaceId=b176c2e3-1a3c-4dfe-9530-0b752538476e&expirationTimestamp=1692705600000&signature=dh3oNyc0Ko88bR9ddh2mDEn0tBg8BfSVVnTM5UVAeLQ&downloadName=imas_theater_icon.png",
+            )
+
+            embeds: list = [init_embed]
+
+            # Embeds composer - used for multiple images
+            if content["images"]:
+                for image in content["images"]:
+                    embeds.append(embed_generator(content, image, color=0xD8A804))
+
+            # Send embed
+            CHANNEL = self.Arisa.get_channel(getenv("channel"))
+            if content["video"] is not None:
+                await CHANNEL.send(files=content["video"], embeds=embeds, silent=True)
+            else:
+                await CHANNEL.send(embeds=embeds, silent=True)
 
 
 def setup(Arisa):
