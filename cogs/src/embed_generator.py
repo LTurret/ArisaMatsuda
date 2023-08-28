b51a66264bb2bf6b6d5029d7dbbfaee9b392b47b
LTurret <a0919329@gmail.com>
2023-08-29 02:56:33 +0800

    image composer fixed

--- cogs/src/embed_generator.py
+++ cogs/src/embed_generator.py
@@ -4,6 +4,15 @@ from interactions import Embed
 
 
 def embed_generator(content: dict, media: str, url: str = "https://arisahi.me", color: hex = 0x1DA0F2) -> Embed:
-    embed = Embed(description=content["full_text"], color=color, timestamp=time(), url=url)
+    embed: Embed = Embed(description=content["full_text"], color=color, timestamp=time(), url=url)
+    embed.set_author(
+        name=f"{content['author']} (@{content['screen_name']})",
+        url=f"https://twitter.com/{content['screen_name']}",
+        icon_url=content["icon_url"],
+    )
+    embed.set_footer(
+        text="樓梯的推特連結修復魔法",
+        icon_url="https://abs.twimg.com/icons/apple-touch-icon-192x192.png",
+    )
     embed.set_image(media)
     return embed
