d4d1b0db1b8551315e347ccd5200f6e86919eb77
LTurret <a0919329@gmail.com>
2023-10-25 00:15:09 +0800

    add init value

--- cogs/src/embed_generator.py
+++ cogs/src/embed_generator.py
@@ -6,8 +6,7 @@ from interactions import Embed
 def embed_generator(
     content: dict,
     media: str,
-    user: str,
-    tweetId: str,
+    tweetId: str | None = None,
     url: str = "https://arisahi.me",
     color: hex = 0x1DA0F2,
     footer_icon: str = "https://abs.twimg.com/icons/apple-touch-icon-192x192.png",
@@ -26,6 +25,6 @@ def embed_generator(
     embed.set_image(media)
 
     if attach_origin_url:
-        embed.add_field(name="原文傳送門", value=f"[點我](https://twitter.com/{user}/status/{tweetId})", inline=False)
+        embed.add_field(name="原文傳送門", value=f"[點我](https://twitter.com/i/status/{tweetId})", inline=False)
 
     return embed
