c09171a550f0983707d5690cb4bbfc9b2af6bff0
LTurret <a0919329@gmail.com>
2023-10-24 22:31:02 +0800

    general code improvement

--- cogs/src/get_contents.py
+++ cogs/src/get_contents.py
@@ -7,26 +7,36 @@ from interactions import File
 
 
 async def get_contents(api_callback: dict) -> dict:
+    
+    # Initialize assets
+    images: list[str] | list = []
+    video: File | None = None
+    media: list | None = None
+    full_text: str | None = None
+    author: str = ""
+    screen_name: str = ""
+    icon_url: str = ""
+    favorite_count: int = -1
+    retweet_count: int = -1
+
     try:
+        # Abbreviations for data accessing
         tweet_detail: dict = api_callback["data"]["tweetResult"]["result"]["legacy"]
-        images: list[str] | list = []
-        video: File | None = None
-        media: list | None = None
-        full_text: str | None = None
-
-        favorite_count: int = tweet_detail["favorite_count"]
-        retweet_count: int = tweet_detail["retweet_count"]
-
         user_results_legacy: dict = api_callback["data"]["tweetResult"]["result"]["core"]["user_results"]["result"]["legacy"]
-        author: str = user_results_legacy["name"]
-        screen_name: str = user_results_legacy["screen_name"]
-        icon_url: str = user_results_legacy["profile_image_url_https"]
+        
+        # Get assets
+        author = user_results_legacy["name"]
+        screen_name = user_results_legacy["screen_name"]
+        icon_url = user_results_legacy["profile_image_url_https"]
+        favorite_count = tweet_detail["favorite_count"]
+        retweet_count = tweet_detail["retweet_count"]
 
         # Extract tweet content text
         if "full_text" in tweet_detail:
             full_text: str = tweet_detail["full_text"]
             start: int = -1
 
+            # Shadowing image URL in tweet text
             if findall(r"https:\/\/t.co\/.+", full_text):
                 match_pop: str = findall(r"https:\/\/t.co\/.+", full_text)[-1]
                 start: int = search(match_pop, full_text).start()
@@ -47,31 +57,33 @@ async def get_contents(api_callback: dict) -> dict:
                             best_bitrate = asset["bitrate"]
                             url = asset["url"]
 
+                # Download video from twitters cdn to discord File object
                 async with ClientSession() as session:
                     async with session.get(url) as response:
                         file: bytes = await response.read()
                         video: File = File(BytesIO(file), file_name="attachment.mp4")
 
-        if "media" in tweet_detail["entities"]:
-            media: list = tweet_detail["entities"]["media"]
-            for image in media:
+        # Check is tweet contains multiple pictures
+        if "media" in tweet_detail["entities"]: 
+            for image in tweet_detail["entities"]["media"]:
                 images.append(image["media_url_https"])
+
     except Exception:
+        print("Ordinary method failed, try api.fxtwitter calling")
         try:
-            images: list[str] | list = []
-            video: File | None = None
-            media: list | None = None
-            full_text: str = f'{api_callback["tweet"]["text"]}'
+            # Abbreviation for data accessing
+            tweet = tweet
 
-            favorite_count: int = api_callback["tweet"]["likes"]
-            retweet_count: int = api_callback["tweet"]["retweets"]
+            # Get assets
+            favorite_count: int = tweet["likes"]
+            retweet_count: int = tweet["retweets"]
+            full_text = f'{tweet["text"]}'
+            author: str = tweet["author"]["name"]
+            screen_name: str = tweet["author"]["screen_name"]
+            icon_url: str = tweet["author"]["avatar_url"]
 
-            author: str = api_callback["tweet"]["author"]["name"]
-            screen_name: str = api_callback["tweet"]["author"]["screen_name"]
-            icon_url: str = api_callback["tweet"]["author"]["avatar_url"]
-
-            if "media" in api_callback["tweet"]:
-                media = api_callback["tweet"]["media"]
+            if "media" in tweet:
+                media = tweet["media"]
 
                 for image in media["photos"]:
                     images.append(image["url"])
