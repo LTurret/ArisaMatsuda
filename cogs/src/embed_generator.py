c6cd25ae290ba744e52dc50183d556f17e24b6b1
LTurret <a0919329@gmail.com>
2023-10-25 00:06:59 +0800

    fix original post button missing issue

--- cogs/src/embed_generator.py
+++ cogs/src/embed_generator.py
@@ -6,9 +6,12 @@ from interactions import Embed
 def embed_generator(
     content: dict,
     media: str,
+    user: str,
+    tweetId: str,
     url: str = "https://arisahi.me",
     color: hex = 0x1DA0F2,
     footer_icon: str = "https://abs.twimg.com/icons/apple-touch-icon-192x192.png",
+    attach_origin_url: bool = False,
 ) -> Embed:
     embed: Embed = Embed(description=content["full_text"], color=color, timestamp=time(), url=url)
     embed.set_author(
@@ -21,4 +24,8 @@ def embed_generator(
         icon_url=footer_icon,
     )
     embed.set_image(media)
+
+    if attach_origin_url:
+        embed.add_field(name="原文傳送門", value=f"[點我](https://twitter.com/{user}/status/{tweetId})", inline=False)
+
     return embed
