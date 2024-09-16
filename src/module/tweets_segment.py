308be436f8857c8e8084a930f5f87e3175c25804
LTurret <a0919329@gmail.com>
2024-09-17 03:29:37 +0800

    ✨ Rework: new module scipts debugging utility.

--- src/module/tweets_segment.py
+++ src/module/tweets_segment.py
@@ -14,37 +14,36 @@ def segment(urls: dict, upper_snowflake: int) -> list:
     return segmented
 
 
-def debug():
-    dummy: list[str] = [
-        "https://twitter.com/imasml_theater/status/1780114650526605616",
-        "https://twitter.com/imasml_theater/status/1780476295002104198",
-        "https://twitter.com/imasml_theater/status/1780521587881746915",
-        "https://twitter.com/imasml_theater/status/1780529132419563615",
-        "https://twitter.com/imasml_theater/status/1780544230815133784",
-        "https://twitter.com/imasml_theater/status/1780551785683542395",
-        "https://twitter.com/imasml_theater/status/1780763174767390756",
-        "https://twitter.com/imasml_theater/status/1780793382438134206",
-        "https://twitter.com/imasml_theater/status/1780838672289436132",
-        "https://twitter.com/imasml_theater/status/1780838923209785686",
-        "https://twitter.com/imasml_theater/status/1780839175052525718",
-        "https://twitter.com/imasml_theater/status/1780839426467258456",
-        "https://twitter.com/imasml_theater/status/1780839678175797395",
-        "https://twitter.com/imasml_theater/status/1780839930073170027",
-        "https://twitter.com/imasml_theater/status/1780840181135724701",
-        "https://twitter.com/imasml_theater/status/1780840432663978312",
-        "https://twitter.com/imasml_theater/status/1780883979526340940",
-        "https://twitter.com/imasml_theater/status/1780884221512253618",
-        "https://twitter.com/imasml_theater/status/1780899079012413613",
-        "https://twitter.com/imasml_theater/status/1781155769544802435",
-        "https://twitter.com/imasml_theater/status/1771488315335667761",
-    ]
-    print(f'Count: {len(segment(dummy, int(input("Enter snowflake: "))))}')
+def debug() -> None:
+    manifest: list[str] = [
+        'https://twitter.com/imasml_theater/status/1833339865162768888',
+        'https://twitter.com/imasml_theater/status/1835332755544056023',
+        'https://twitter.com/imasml_theater/status/1834651886487531848',
+        'https://twitter.com/imasml_theater/status/1834608474866811266',
+        'https://twitter.com/imasml_theater/status/1834608223997104459',
+        'https://twitter.com/imasml_theater/status/1834607984225443903',
+        'https://twitter.com/imasml_theater/status/1834502526705823838',
+        'https://twitter.com/imasml_theater/status/1834502277157322822',
+        'https://twitter.com/imasml_theater/status/1834396578792308773',
+        'https://twitter.com/imasml_theater/status/1834207837343383963',
+        'https://twitter.com/imasml_theater/status/1834206809785307630',
+        'https://twitter.com/imasml_theater/status/1834206000456630375',
+        'https://twitter.com/imasml_theater/status/1834080495354687699',
+        'https://twitter.com/imasml_theater/status/1834080243684180399',
+        'https://twitter.com/imasml_theater/status/1834079992180867578',
+        'https://twitter.com/imasml_theater/status/1834079740715786539',
+        'https://twitter.com/imasml_theater/status/1834079491116658783',
+        'https://twitter.com/imasml_theater/status/1834064391870054608',
+        'https://twitter.com/imasml_theater/status/1834034195288691043',
+        'https://twitter.com/imasml_theater/status/1833831119537741890',
+        'https://twitter.com/imasml_theater/status/1833792850490007757'
+        ] 
+    logging.debug(f'Count: {len(segment(manifest, int(input("Enter upper snowflake: "))))}')
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, format="%(message)s ", datefmt="%Y-%m-%d %H:%M:%S")
     try:
         debug()
-        print("\033[93m")  # Format warning
     except Exception as exception:
-        print(exception)
-    raise Warning(f"{__file__} Running with debug mode, Please note that this script is not tend to run independently.")
+        logging.debug(exception)
