580ac4b11a6aeb5b7191bb354daf0e57b11c7847
LTurret <a0919329@gmail.com>
2025-03-07 02:14:27 +0800

    🐛 Fix Incomplete regular expression for hostnames

--- src/module/get_tokens.py
+++ src/module/get_tokens.py
@@ -7,7 +7,7 @@ async def get_tokens() -> dict:
     async with ClientSession() as session:
         async with session.get("https://twitter.com") as response:
             response: str = await response.text()
-            js_url: str = findall(r"https://abs.twimg.com/responsive-web/client-web-legacy/main.[^\.]+.js", response)[0]
+            js_url: str = findall(r"https://abs\.twimg\.com/responsive-web/client-web-legacy/main\.[^\.]+\.js", response)[0]
         async with session.get(js_url) as mainjs:
             mainjs: str = await mainjs.text()
             bearer_token: str = findall(r'AAAAAAAAA[^"]+', mainjs)[0]
